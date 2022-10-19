6cc919dc4df50d413c7077573c6c43d5fd5d81c8
gustavosalviato <gustavosalviato8@gmail.com>
2022-10-18 21:04:56 -0300

    interface extension

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,21 +1,25 @@
 import styles from './Avatar.module.css'
+import { ImgHTMLAttributes } from 'react'
 
-interface AvatarProps {
-    src: string
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean
     hasMargin?: boolean
 }
 
-export const Avatar = ({ src, hasBorder = true, hasMargin }: AvatarProps) => {
+export const Avatar = ({ hasBorder = true, hasMargin, ...props }: AvatarProps) => {
     return (
         <img
+            {...props}
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-            src={src}
-            style={{
-                margin: hasMargin ? '0 auto' : '0',
-                marginTop: hasMargin ? 'calc(-4.5rem / 2)' : '0',
-                marginBottom: hasMargin ? '1rem' : '0',
-            }}
+            style={
+                {
+                    margin: hasMargin ? '0 auto' : '0',
+                    marginTop: hasMargin ? 'calc(-4.5rem / 2)' : '0',
+
+                    marginBottom: hasMargin ? '1rem' : '0',
+                }
+            }
+
 
         />
     )
